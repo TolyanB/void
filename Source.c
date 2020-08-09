dd783f620cf1b074106f205b10dff09a824cb843
TolyanB <60188766+TolyanB@users.noreply.github.com>
2020-08-09 17:22:12 +0300

    Update Source.c

--- Source.c
+++ Source.c
@@ -10,13 +10,13 @@ const void* getMax(const void* const x1, const void* const x2, char const type)
 	const void* result = NULL;
 	switch (type)
 	{
-	case 'd': result = *((double*)x1) > * ((double*)x2) ? x1 : x2;
+	case 'd': result = *((double*)x1) > *((double*)x2) ? x1 : x2;
 		break;
-	case 'i': result = *((int*)x1) > * ((int*)x2) ? x1 : x2;
+	case 'i': result = *((int*)x1) > *((int*)x2) ? x1 : x2;
 		break;
-	case 'u': result = *((unsigned*)x1) > * ((unsigned*)x2) ? x1 : x2;
+	case 'u': result = *((unsigned*)x1) > *((unsigned*)x2) ? x1 : x2;
 		break;
-	case 'c': result = *((char*)x1) > * ((char*)x2) ? x1 : x2;
+	case 'c': result = *((char*)x1) > *((char*)x2) ? x1 : x2;
 		break;
 	}
 
